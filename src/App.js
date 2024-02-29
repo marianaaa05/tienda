a88f6ae2f15c8b535d388ce1a7670f42bcd1f769
marianaaa05 <143300301+marianaaa05@users.noreply.github.com>
2024-02-29 16:22:23 -0500

    Creacion proyecto tienda

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+  return(
+    <BrowserRouter>
+      <div className='flex relative dark: bg-main-dark-bg'>
+        <div className='fixed right-4 button-4' style={{zIndex:'1000'}}>
+          <TooltipComponent content="Settings" position='Top'>
+            <button
+              type='button'
+              className='text-3xl p-3 hover:drop-shadow hover:bg-light-gray text-white'
+              style={{background: 'blue',borderRadius: '50%'}}
+            
+            >
+              <FiSettings/>
+            </button>
+          </TooltipComponent>
+        </div>
+      </div>
 
-export default App;
+    </BrowserRouter>
+  );
+};
