bf3e693351dd3995823dc4dfc8de481557005ad8
marianaaa05 <143300301+marianaaa05@users.noreply.github.com>
2024-03-11 15:33:51 -0500

    video 2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,97 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { FiSettings } from 'react-icons/fi';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { Navbar, Footer, ThemeSettings, Sidebar } from "./components/index";
+import {
+  Ecommerce,
+  Orders,
+  Calendar,
+  Employees,
+  Customers,
+  Kanban,
+  Editor,
+} from "./pages";
+import LineChart from "./pages/Charts/Line";
+import AreaChart from "./pages/Charts/Area";
+import BarChart from "./pages/Charts/Bar";
+import PieChart from "./pages/Charts/Pie";
+import FinancialChart from "./pages/Charts/Financial";
+import ColorPicker from "./pages/ColorPicker";
+import ColorMappingChart from "./pages/Charts/ColorMapping";
+import PyramidChart from "./pages/Charts/Pyramid";
+import StackedChart from "./pages/Charts/Stacked";
 
+import "./App.css";
 
-
-import './App.css';
-
-/*Define un componente*/ 
 export const App = () => {
-  return(
+  const activeMenu = false;
+  return (
     <BrowserRouter>
-      <div className='flex relative dark: bg-main-dark-bg'>
-        <div className='fixed right-4 button-4' style={{zIndex:'1000'}}>
-          <TooltipComponent content="Settings" position='Top'>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+          <TooltipComponent content="settings" position="Top">
             <button
-              type='button'
-              className='text-3xl p-3 hover:drop-shadow hover:bg-light-gray text-white'
-              style={{background: 'violet',borderRadius: '50%'}}
-            
+              type="button"
+              className="text-3xl p-3 hover:drop-shadow-x1 hover:bg-light-gray text-white"
+              style={{
+                background: "gray",
+                borderRadius: "50%",
+                position: "fixed",
+                bottom: "20px",
+                right: "20px",
+                zIndex: "1000",
+              }}
             >
-              <FiSettings/>
+              <FiSettings />
             </button>
           </TooltipComponent>
         </div>
-
-
+        {activeMenu ? (
+          <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+            <Sidebar></Sidebar>
+          </div>
+        ) : (
+          <div className="w-0  dark:bg-secondary-dark-bg">
+            <Sidebar></Sidebar>
+          </div>
+        )}
+        <div
+          className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+            activeMenu ? "md-ml-72" : "flex-2"
+          }`}
+        >
+          <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+            <Navbar></Navbar>
+          </div>
+          {/* ----------------------- */}
+          <div>
+            <Routes>
+              {/*dashBoard*/}
+              <Route path="/" element={<Ecommerce />} />
+              <Route path="/ecommerce" element={<Ecommerce />} />
+              {/* pages*/}
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/employees" element={<Employees />} />
+              <Route path="/customers" element={<Customers />} />
+              {/* Apps */}
+              <Route path="/kanban" element={<Kanban />} />
+              <Route path="/editor" element={<Editor />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/color-picker" element={<ColorPicker />} />
+              {/* Graficos */}
+              <Route path="/line" element={<LineChart />} />
+              <Route path="/area" element={<AreaChart />} />
+              <Route path="/bar" element={<BarChart />} />
+              <Route path="/pie" element={<PieChart />} />
+              <Route path="/financial" element={<FinancialChart />} />
+              <Route path="/color-mapping" element={<ColorMappingChart />} />
+              <Route path="/pyramid" element={<PyramidChart />} />
+              <Route path="/stacked" element={<StackedChart />} />
+            </Routes>
+          </div>
+        </div>
       </div>
-
     </BrowserRouter>
   );
 };
