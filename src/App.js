5dcaed2313d50449475ce0b4d830d0c61c1a97dd
marianaaa05 <143300301+marianaaa05@users.noreply.github.com>
2024-04-20 19:10:55 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,87 +1,143 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { FiSettings } from "react-icons/fi";
-import { TooltipComponent } from "@syncfusion/ej2-react-popups";
-import { Navbar, Footer, ThemeSettings, Sidebar } from "./components/index";
-import {
-  Ecommerce,
-  Orders,
-  Calendar,
-  Employees,
-  Customers,
-  Kanban,
-  Editor,
-} from "./pages";
-import LineChart from "./pages/Charts/Line";
-import AreaChart from "./pages/Charts/Area";
-import BarChart from "./pages/Charts/Bar";
-import PieChart from "./pages/Charts/Pie";
-import FinancialChart from "./pages/Charts/Financial";
-import ColorPicker from "./pages/ColorPicker";
-import ColorMappingChart from "./pages/Charts/ColorMapping";
-import PyramidChart from "./pages/Charts/Pyramid";
-import StackedChart from "./pages/Charts/Stacked";
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { BsCurrencyDollar } from 'react-icons/bs'
+import { VscPrimitiveSquare } from 'react-icons/vsc'
+import { Stacked, Pie, Button, SparkLine } from '../components'
+import { earningData, SparklineAreaData, ecomPieChartDat } from '../data/dummy'
+import { useStateContext } from '../contexts/ContextProvider'
 
-import "./App.css";
-import { useStateContext } from './contexts/ContextProvider'
-
-export const App = () => {
-  const { activeMenu } = useStateContext();
-
+function Ecommerce () {
+  const { currentColor } = useStateContext()
   return (
-    <BrowserRouter>
-      <div className="flex relative dark:bg">
-        <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
-          <TooltipComponent content="settings" position="Top">
+    <div className='mt-24'>
+      <div className='flex flex-wrap lg:flex-nowrap justify-center'>
+        <div className='bg-white data:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center'>
+          <div className='flex justify-between items-center'>
+            <div>
+              <p className='font-bold'>Earnings Datas</p>
+              <p className='text-2xl'>$58.909.786</p>
+              <earningData />
+            </div>
             <button
-              type="button"
-              className="text-3xl p-3 hover:drop-shadow-x1 hover:bg-light-gray text-white"
-              style={{ background: "#333", borderRadius: "50%" }}
+              type='button'
+              style={{ backgroundColor: currentColor }}
+              className='text-2xl opacity-0.9 text-white hover:drop-shadow-xl rounded-full p-4'
             >
-              <FiSettings />
+              <BsCurrencyDollar />
             </button>
-          </TooltipComponent>
+          </div>
+          <div>
+            <Button
+              color='white'
+              bgColor={currentColor}
+              text='Download'
+              borderRadius='10px'
+              size='md'
+            />
+          </div>
         </div>
-        {activeMenu ? (
-          <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-            <Sidebar />
-          </div>
-        ) : (
-          <div className="w-0  dark:bg-secondary-dark-bg">
-            <Sidebar />
-          </div>
-        )}
-        <div
-          className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}
-        >
-          <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-            <Navbar />
-          </div>
-          <Routes>
-            {/* dashBoard */}
-            <Route path="/" element={<Ecommerce />} />
-            <Route path="/ecommerce" element={<Ecommerce />} />
-            {/* pages */}
-            <Route path="/orders" element={<Orders />} />
-            <Route path="/employees" element={<Employees />} />
-            <Route path="/customers" element={<Customers />} />
-            {/* Apps */}
-            <Route path="/kanban" element={<Kanban />} />
-            <Route path="/editor" element={<Editor />} />
-            <Route path="/calendar" element={<Calendar />} />
-            <Route path="/color-picker" element={<ColorPicker />} />
-            {/* Graficos */}
-            <Route path="/line" element={<LineChart />} />
-            <Route path="/area" element={<AreaChart />} />
-            <Route path="/bar" element={<BarChart />} />
-            <Route path="/pie" element={<PieChart />} />
-            <Route path="/financial" element={<FinancialChart />} />
-            <Route path="/color-mapping" element={<ColorMappingChart />} />
-            <Route path="/pyramid" element={<PyramidChart />} />
-            <Route path="/staked" element={<StackedChart />} />
-          </Routes>
+        <div className='flex  m-3 flex-wrap justify-center gap-1 items-center'>
+          {earningData.map((item) => (
+            <div
+              key={item.title}
+              className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl hover:drop-shadow-xl'
+            >
+              <button
+                type='button'
+                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                className='text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl'
+              >
+                {item.icon}
+              </button>
+              <p className='mt-3'>
+                <span className='text-lg font-semibold'>
+                  {item.amount}
+                </span>
+                <span className={`text-sm text-${item.color} ml-2`}>
+                  {item.percentage}
+                </span>
+              </p>
+              <p className='text-sm text-gray-400 mt-1'>
+                {item.title}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
-    </BrowserRouter>
+      {/* nueva seccion */}
+      <div className='flex gap-10 flex-wrap justify-center'>
+        <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-788'>
+          <div className='flex justify-between'>
+            <p className='font-semibold text-xl'>
+              Ingresos
+            </p>
+            <div className='fle items-center gap-4'>
+              <p className='flex items-center gap-2 text-gray-600 hover:drop-shadow-xl'>
+                <span>
+                  <VscPrimitiveSquare />
+                </span>
+                <span>Gastos</span>
+              </p>
+              <p className='flex items-center gap-2 text-green-600 hover:drop-shadow-xl'>
+                <span>
+                  <VscPrimitiveSquare />
+                </span>
+                <span>Presupuesto</span>
+              </p>
+            </div>
+          </div>
+          <div className='mt-10 flex gap-10 flex-wrap justify-center'>
+            <div className='border-r-1 border-color m-4 pr-10'>
+              <div>
+                <p>
+                  <span className='text-3xl font-semibold'>
+                    $76.897.543
+                  </span>
+                  <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-600 ml-3 text-xs'>
+                    30%
+                  </span>
+                </p>
+                <p className='text-gra-500 mt-1'>Presupuesto</p>
+              </div>
+              <div className='mt-8'>
+                <p>
+                  <span className='text-3xl font-semibold'>
+                    $18.998.456
+                  </span>
+                </p>
+                <p className='text-gray-500 mt-1'>Gastos</p>
+              </div>
+              <div>
+                <SparkLine
+                  currentColor='gray'
+                  id='line-sparkLine'
+                  type='Line'
+                  height='80px'
+                  width='250px'
+                  data={SparklineAreaData}
+                  color={currentColor}
+                />
+              </div>
+              <div className='mt-10'>
+                <Button
+                  color='white'
+                  bgColor={currentColor}
+                  text='Download Report'
+                  borderRadius='10px'
+                />
+              </div>
+            </div>
+            <div>
+              <Stacked
+                width='320px'
+                height='360px'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
+
+export default Ecommerce
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const Button = () => {
-  return (
-    <div>Button</div>
+const Button = ({ bgColor, color, size, text, borderRadius }) => {
+  return(
+    <button 
+    type='button'
+    style={{ backgroundColor: bgColor, color, borderRadius}}
+    className={`text-${size} p-3 hover:drop-shadow-xm`}
+    >
+      {text}
+
+    </button>
   )
 }
+
+
 
 export default Button
